8e1eefc1041e04acb91f94608644753f72321d13
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-22 20:09:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import Section from './Feedback/Section';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Statistics from './Feedback/Statistics';
